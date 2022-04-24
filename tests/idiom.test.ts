682c0ffd0ea43de568e1038b9f4cb8addb335c09
Łukasz Klęba <lukasz.kleba99@onet.pl>
2022-04-24 20:05:52 +0200

    idiom.tests.ts

--- tests/idiom.test.ts
+++ tests/idiom.test.ts
@@ -1,13 +1,46 @@
 import {IdiomRecord} from "../records/idiom.record";
+import {pool} from "../utils/connection";
+
 
 let idiom : IdiomRecord;
 
 beforeAll(()=>{
     idiom = new IdiomRecord({
-        idiom:"Testowy"
+        idiom:"Testing"
     })
+})
+
+afterAll(async()=>{
+    await pool.end();
 })
 
 test ('created idiom should has defined id',()=>{
     expect(idiom.id).toBeDefined();
+})
+
+test('inserted idiom should exist in database',async()=>{
+    const id =await idiom.insert();
+    const record = await IdiomRecord.getOne(id);
+    expect(record).toBeDefined();
+    await record.delete();
+})
+
+test('deleted idiom shouldnt exist in database',async()=>{
+    const id =await idiom.insert();
+    const record = await IdiomRecord.getOne(id);
+    await record.delete();
+    const deletedRecord = await IdiomRecord.getOne(id);
+    expect(deletedRecord).toBeNull();
+})
+
+test('updated record should be different than before updating',async ()=>{
+    const id =await idiom.insert();
+    const record = await IdiomRecord.getOne(id);
+    const oldRecord = {...record};
+    await record.update({
+        idiom:"Updated Testing"
+    })
+    const newRecord = await IdiomRecord.getOne(id);
+    expect(newRecord).not.toBe(oldRecord);
+    await newRecord.delete();
 })
